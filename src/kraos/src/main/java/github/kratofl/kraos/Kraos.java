2c05498f1b49463cfc4856786b751a122c2c03f8
kratofl <l.achler@mopsoft.de>
2024-09-17 21:14:36 +0200

    fixed davids fuck up

--- src/kraos/src/main/java/github/kratofl/kraos/Kraos.java
+++ src/kraos/src/main/java/github/kratofl/kraos/Kraos.java
@@ -1,8 +1,8 @@
-package kratofl.kraos;
+package github.kratofl.kraos;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Kraos extends JavaPlugin {
 
     @Override
     public void onEnable() {
