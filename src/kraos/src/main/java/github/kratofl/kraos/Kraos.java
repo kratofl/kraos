6af7aacf84f03b6133755dc6767d5007123ebf56
Dm4urer <Dmaurer870@gmail.com>
2024-09-17 22:15:09 +0200

    First plugin

--- src/kraos/src/main/java/github/kratofl/kraos/Kraos.java
+++ src/kraos/src/main/java/github/kratofl/kraos/Kraos.java
@@ -1,5 +1,7 @@
 package github.kratofl.kraos;
 
+import github.kratofl.kraos.commands.HomeCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Kraos extends JavaPlugin {
@@ -7,7 +9,7 @@ public final class Kraos extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getPluginCommand("home").setExecutor(new HomeCommand());
     }
 
     @Override
